96051bfe8551f0dbcc5e4591717e81b996c142cb
Igor <igor.kowalczyk@cyfroweszkoly.pl>
2025-05-08 09:44:16 +0200

    add timer for answer (CountDownTimer)

--- app/src/main/java/com/example/personality_quiz/MainActivity.kt
+++ app/src/main/java/com/example/personality_quiz/MainActivity.kt
@@ -1,51 +1,44 @@
 package com.example.personality_quiz
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.SystemClock
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Chronometer
-import android.widget.Spinner
+import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var running = false
+    private var pauseOffset: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Spinner
         val spinner: Spinner = findViewById(R.id.spinnerCountries)
         val countries = arrayOf("Polska", "Nauru", "Tuvalu", "Eswatini")
-        val adapter = ArrayAdapter( this,
-            android.R.layout.simple_list_item_1,
-            countries
-        )
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
 
-
-        lateinit var chronometer: Chronometer
-        lateinit var startBtn: Button
-        lateinit var stopBtn: Button
-        lateinit var resetBtn: Button
-        var running = false
-        var pauseOffset: Long = 0
-
-        chronometer = findViewById<Chronometer>(R.id.myChronometer)
-        startBtn = findViewById(R.id.startButton)
-        stopBtn = findViewById(R.id.stopButton)
-
+        // Chronometer
+        val chronometer: Chronometer = findViewById(R.id.myChronometer)
+        val startBtn: Button = findViewById(R.id.startButton)
+        val stopBtn: Button = findViewById(R.id.stopButton)
 
         startBtn.setOnClickListener {
-
             if (!running) {
                 chronometer.base = SystemClock.elapsedRealtime() - pauseOffset
                 chronometer.start()
@@ -62,5 +55,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // CountDown Timer Example
+        val timerTextView: TextView = findViewById(R.id.timerCounter)
+
+        val countDownTimer = object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsLeft = millisUntilFinished / 1000
+                timerTextView.text = "Pozostało $secondsLeft sekund"
+            }
+
+            override fun onFinish() {
+                timerTextView.text = "Czas minął!"
+            }
+        }
+
+        countDownTimer.start()
     }
 }
